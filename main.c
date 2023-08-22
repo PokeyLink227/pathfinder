87348fc13f457bd78a442da9114ad099e9a6d1f6
Thomas Rice <thomasrice7@yahoo.com>
2023-08-22 01:59:48 -0400

    improved lerp, floating point error

--- main.c
+++ main.c
@@ -112,10 +112,26 @@ int dist(int i1, int i2, int width) {
 }
 
 int maxi_mag(int lhs, int rhs) {
+    printf("%i  %i -> ", lhs, rhs);
     if (lhs < 0) lhs = -lhs;
     if (rhs < 0) rhs = -rhs;
-    if (lhs > rhs) return lhs;
-    else return rhs;
+    printf("%i  %i -> ", lhs, rhs);
+    if (lhs > rhs) {
+        printf("%i\n", lhs);
+        return lhs;
+    }
+    else {
+        printf("%i\n", rhs);
+        return rhs;
+    }
+}
+
+char ContainsPoint(GridData gd, Point2D pt) {
+    return (pt.x >= 0 && pt.x < gd.width && pt.y >= 0 && pt.y < gd.height);
+}
+
+char UI_ContainsPoint(UI_Element ele, Point2D pt) {
+    return (pt.x >= ele.x && pt.x < ele.x + ele.width && pt.y >= ele.y && pt.y < ele.y + ele.height);
 }
 
 // ALGORITHMS
@@ -216,28 +232,19 @@ int AStarFull(GridData gd, NList *openList) {
     return ALGO_COMPLETE;
 }
 
-Point2D WintoWorld(Point2D pt, Point2D cam, GridData gd) {
-    pt.x -= cam.x;
-    pt.y -= cam.y;
-    return (Point2D){0, 0};
-}
-
-char ContainsPoint(GridData gd, Point2D pt) {
-    return (pt.x >= 0 && pt.x < gd.width && pt.y >= 0 && pt.y < gd.height);
-}
-
 int main() {
     int screen_width = 800, screen_height = 800;
     InitWindow(screen_width, screen_height, "A-Star Demo");
-    SetTargetFPS(144);
+    SetTargetFPS(60);
 
-    UI_Element camera = {0, 0, 800, 700, 0, UI_WINDOW, UI_INVISIBLE, UI_NONINTERACTABLE}; // this is stupid
+    UI_Element menu_area = {0, 0, 800, 100, 0, UI_WINDOW, UI_INVISIBLE, UI_NONINTERACTABLE};
+    UI_Element window_area = {0, 100, 800, 700, 0, UI_WINDOW, UI_INVISIBLE, UI_NONINTERACTABLE};
     UI_Element buttons[8] = {
         (UI_Element){10, 10, 90, 35, "Start", UI_BUTTON, UI_VISIBLE, UI_INTERACTABLE},
         (UI_Element){10, 55, 90, 35, "Clear All", UI_BUTTON, UI_VISIBLE, UI_INTERACTABLE},
         (UI_Element){110, 55, 90, 35, "Clear Walls", UI_BUTTON, UI_VISIBLE, UI_INTERACTABLE},
         (UI_Element){210, 55, 90, 35, "Clear Paths", UI_BUTTON, UI_VISIBLE, UI_INTERACTABLE},
-        (UI_Element){0},
+        (UI_Element){110, 10, 90, 35, "asd", UI_BUTTON, UI_VISIBLE, UI_INTERACTABLE},
         (UI_Element){0},
         (UI_Element){0},
         (UI_Element){0}
@@ -247,7 +254,6 @@ int main() {
     // color legend for grid
     Color color_palette[8] = {WHITE, LIGHTGRAY, GREEN, BLACK, YELLOW, RED, BLUE};
 
-
     Node grid[10000];
     GridData gd = {grid, 100, 100, 10000, 982, 1275};
     for (int i = 0; i < gd.size; i++) grid[i] = (Node){-1, -1, -1, -1, LOC_GRID};
@@ -255,29 +261,21 @@ int main() {
     int open_raw[10000];
     NList openList = (NList){open_raw, 0, 10000};
 
-
     int count = 0, found = 0;
-    int mouse_x_last = 0, mouse_y_last = 0, mouse_x = 0, mouse_y = 0;
     char left_mouse_pressed = 0, dragging_start = 0, dragging_end = 0;
 
-
-
-    int cbx = 0, cbw = 800, cby = 100, cbh = 800;
-    int mbx = 0, mbw = 800, mby = 000, mbh = 100;
-    char drag = 0; // 0:none, 1:start, 2:end
-
+    Point2D mouse = {0, 0}, mouse_last = {0, 0}, camera = {0, 0};
 
     while (!WindowShouldClose()) {
-        mouse_x_last = mouse_x;
-        mouse_y_last = mouse_y;
-        mouse_x = GetMouseX();
-        mouse_y = GetMouseY();
-
+        mouse_last.x = mouse.x;
+        mouse_last.y = mouse.y;
+        mouse.x = GetMouseX();
+        mouse.y = GetMouseY();
 
 
         if (IsKeyDown(KEY_RIGHT)) {
             camera.x += 10;
-            if (camera.x + camera.width > gd.width * cell_width) camera.x = gd.width * cell_width - camera.width;
+            if (camera.x + window_area.width > gd.width * cell_width) camera.x = gd.width * cell_width - window_area.width;
         }
         if (IsKeyDown(KEY_LEFT)) {
             camera.x -= 10;
@@ -289,13 +287,13 @@ int main() {
         }
         if (IsKeyDown(KEY_DOWN)) {
             camera.y += 10;
-            if (camera.y + camera.height > gd.height * cell_width) camera.y = gd.height * cell_width - camera.height;
+            if (camera.y + window_area.height > gd.height * cell_width) camera.y = gd.height * cell_width - window_area.height;
         }
 
 
         if (IsMouseButtonDown(0)) {
-            if (mouse_x >= mbx && mouse_x < mbw && mouse_y >= mby && mouse_y < mbh) {
-                for (int i = 0; i < 4; i++) if (!left_mouse_pressed && mouse_x >= buttons[i].x && mouse_x < buttons[i].x + buttons[i].width && mouse_y >= buttons[i].y && mouse_y < buttons[i].y + buttons[i].height) {
+            if (UI_ContainsPoint(menu_area, mouse)) {
+                for (int i = 0; i < 4; i++) if (!left_mouse_pressed && UI_ContainsPoint(buttons[i], mouse)) {
                     printf("[%s] button clicked\n", buttons[i].text);
                     if (i == 0 && !found) { // start button
                         found = AStarFull(gd, &openList);
@@ -320,32 +318,32 @@ int main() {
                     }
                 }
             }
-            else if (mouse_x >= cbx && mouse_x < cbw && mouse_y >= cby && mouse_y < cbh) { // clicked on canvas area
-                int pos = (mouse_y + camera.y - 100) / cell_width * gd.width + (mouse_x + camera.x) / cell_width;
+            else if (UI_ContainsPoint(window_area, mouse)) { // clicked on canvas area
+                int pos = (mouse.y + camera.y - 100) / cell_width * gd.width + (mouse.x + camera.x) / cell_width;
 
                 if (IsKeyDown(KEY_SPACE)) {
-                    camera.x += mouse_x_last - mouse_x;
-                    camera.y += mouse_y_last - mouse_y;
+                    camera.x += mouse_last.x - mouse.x;
+                    camera.y += mouse_last.y - mouse.y;
 
-                    if (camera.x + camera.width > (gd.width - 1) * cell_width + screen_width) camera.x = (gd.width - 1) * cell_width;
-                    if (camera.x < cell_width - camera.width) camera.x = cell_width - camera.width;
-                    if (camera.y + camera.height > (gd.height - 1) * cell_width + camera.height) camera.y = (gd.height - 1) * cell_width;
-                    if (camera.y < cell_width - camera.height) camera.y = cell_width - camera.height;
+                    if (camera.x + window_area.width > (gd.width - 1) * cell_width + screen_width) camera.x = (gd.width - 1) * cell_width;
+                    if (camera.x < cell_width - window_area.width) camera.x = cell_width - window_area.width;
+                    if (camera.y + window_area.height > (gd.height - 1) * cell_width + window_area.height) camera.y = (gd.height - 1) * cell_width;
+                    if (camera.y < cell_width - window_area.height) camera.y = cell_width - window_area.height;
                 }
-                else if (dragging_start) gd.start_pos = (mouse_y + camera.y - 100) / cell_width * gd.width + (mouse_x + camera.x) / cell_width;
-                else if (dragging_end) gd.end_pos = (mouse_y + camera.y - 100) / cell_width * gd.width + (mouse_x + camera.x) / cell_width;
+                else if (dragging_start) gd.start_pos = (mouse.y + camera.y - 100) / cell_width * gd.width + (mouse.x + camera.x) / cell_width;
+                else if (dragging_end) gd.end_pos = (mouse.y + camera.y - 100) / cell_width * gd.width + (mouse.x + camera.x) / cell_width;
                 else if (!left_mouse_pressed && pos == gd.start_pos) dragging_start = 1;
                 else if (!left_mouse_pressed && pos == gd.end_pos) dragging_end = 1;
                 else {
-                    int dist = maxi_mag((mouse_x / cell_width - mouse_x_last / cell_width), (mouse_y / cell_width - mouse_y_last / cell_width));
+                    Point2D wpos = {mouse.x + camera.x, mouse.y + camera.y - 100}, wpos_last = {mouse_last.x + camera.x, mouse_last.y + camera.y - 100};
+                    int dist = maxi_mag((wpos.x / cell_width - wpos_last.x / cell_width), (wpos.y / cell_width - wpos_last.y / cell_width));
                     for (int i = 0; i <= dist; i++) {
                         float t = dist == 0 ? 0 : (float)i / (float)dist;
-                        int lerp_x = ((float)mouse_x_last * (1.0f - t) + (float)mouse_x * t + camera.x) / cell_width,
-                        lerp_y = ((float)mouse_y_last * (1.0f - t) + (float)mouse_y * t + camera.y - 100) / cell_width;
+                        int lerp_x = ((float)wpos_last.x / cell_width * (1.0f - t) + (float)wpos.x / cell_width * t),
+                        lerp_y = ((float)wpos_last.y / cell_width * (1.0f - t) + (float)wpos.y / cell_width * t);
                         if (ContainsPoint(gd, (Point2D){lerp_x, lerp_y}) && lerp_y * gd.width + lerp_x != gd.start_pos && lerp_y * gd.width + lerp_x != gd.end_pos) grid[lerp_y * gd.width + lerp_x].loc = LOC_WALL;
                     }
                 }
-
             }
             left_mouse_pressed = 1;
 
@@ -364,16 +362,16 @@ int main() {
             int cell_width_previous = cell_width;
             cell_width += size_diff;
             if (cell_width > 50) cell_width = 50;
-            //if (cell_width * gd.width < camera.width) cell_width = camera.width / gd.width;
-            //if (cell_width * gd.height < camera.height) cell_width = camera.height / gd.height;
+            //if (cell_width * gd.width < window_area.width) cell_width = window_area.width / gd.width;
+            //if (cell_width * gd.height < window_area.height) cell_width = window_area.height / gd.height;
             if (cell_width < 1) cell_width = 1;
 
-            camera.x = (camera.x + mouse_x) * cell_width / cell_width_previous - mouse_x;
-            camera.y = (camera.y + mouse_y) * cell_width / cell_width_previous - mouse_y;
-            if (camera.x + camera.width > (gd.width - 1) * cell_width + screen_width) camera.x = (gd.width - 1) * cell_width;
-            if (camera.x < cell_width - camera.width) camera.x = cell_width - camera.width;
-            if (camera.y + camera.height > (gd.height - 1) * cell_width + camera.height) camera.y = (gd.height - 1) * cell_width;
-            if (camera.y < cell_width - camera.height) camera.y = cell_width - camera.height;
+            camera.x = (camera.x + mouse.x) * cell_width / cell_width_previous - mouse.x;
+            camera.y = (camera.y + mouse.y) * cell_width / cell_width_previous - mouse.y;
+            if (camera.x + window_area.width > (gd.width - 1) * cell_width + screen_width) camera.x = (gd.width - 1) * cell_width;
+            if (camera.x < cell_width - window_area.width) camera.x = cell_width - window_area.width;
+            if (camera.y + window_area.height > (gd.height - 1) * cell_width + window_area.height) camera.y = (gd.height - 1) * cell_width;
+            if (camera.y < cell_width - window_area.height) camera.y = cell_width - window_area.height;
 
         }
 
@@ -383,7 +381,7 @@ int main() {
         BeginDrawing();
             ClearBackground(BLACK);
             for (int i = 0; i < gd.size; i++) {
-                if (i % gd.width * cell_width >= camera.x - cell_width && i % gd.width * cell_width <= camera.x + camera.width + cell_width && i / gd.width * cell_width >= camera.y - cell_width && i / gd.width * cell_width <= camera.y + camera.height + cell_width) {
+                if (i % gd.width * cell_width >= camera.x - cell_width && i % gd.width * cell_width <= camera.x + window_area.width + cell_width && i / gd.width * cell_width >= camera.y - cell_width && i / gd.width * cell_width <= camera.y + window_area.height + cell_width) {
                     DrawRectangle(i % gd.width * cell_width - camera.x, 100 + i / gd.width * cell_width - camera.y, cell_width, cell_width, color_palette[grid[i].loc]);
                 }
             }
